17c85fc80f31d002cd1b4df219c0ee5c22ccaed3
Andre Weissflog <floooh@gmail.com>
2019-04-23 20:20:47 +0200

    fips code generation scripts

---
+++ fips-files/generators/SokolShader.py
@@ -0,0 +1,48 @@
+#-------------------------------------------------------------------------------
+#   SokolShader.py
+#
+#   Fips code-generator script for invoking oryol-shdc during the build.
+#
+#   Use the cmake macro 'sokol_shader(glsl-file)' inside a fips application
+#   target (fips_begin_* / fips_end_*) to hook the code-generation
+#   build job into the build process.
+#
+#-------------------------------------------------------------------------------
+
+Version = 1
+
+import os, platform, subprocess
+import genutil as util
+from mod import log
+
+#-------------------------------------------------------------------------------
+def find_shdc():
+    shdc_path = os.path.dirname(os.path.abspath(__file__))
+    shdc_path += '/../../bin/'
+    if platform.system() == 'Windows':
+        shdc_path += 'win32/'
+    elif platform.system() == 'Darwin':
+        shdc_path += 'osx/'
+    elif platform.system() == 'Linux':
+        if os.uname()[1] == 'raspberrypi':
+            shdc_path += 'raspi/'
+        else:
+            shdc_path += 'linux/'
+    else:
+        log.error('Unknown host system {}'.format(platform.system()))
+    return shdc_path + 'sokol-shdc'
+
+#-------------------------------------------------------------------------------
+def generate(input, out_src, out_hdr, args):
+    errfmt = 'msvc' if args['compiler']=='MSVC' else 'gcc'
+    if util.isDirty(Version, [input], [out_hdr]):
+        cmd = [find_shdc(), 
+                '--input', input,
+                '--output', out_hdr,
+                '--slang', args['slang'],
+                '--genver', str(Version),
+                '--errfmt', errfmt,
+                '--noifdef']
+        res = subprocess.call(cmd)
+        if res != 0:
+            log.error('sokol-shdc return with error {}'.format(res))
--- fips-files/include.cmake
+++ fips-files/include.cmake
@@ -1,3 +1,5 @@
-macro(sokol_shader shd)
-    message("sokol_shader ${shd} called!")
+macro(sokol_shader shd slang)
+    # FIXME: debug shaders
+    set(args "{slang: '${slang}', compiler: '${CMAKE_C_COMPILER_ID}' }")
+    fips_generate(TYPE SokolShader FROM ${shd} HEADER ${shd}.h OUT_OF_SOURCE ARGS ${args})
 endmacro()
